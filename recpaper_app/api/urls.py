0ea6c5bb50f6bf68d4e3edaede62526541aa90b7
Gautam Maurya <gautammaurya1299@gmail.com>
2025-04-24 14:25:45 +0530

    modify view methos to view classes

--- recpaper_app/api/urls.py
+++ recpaper_app/api/urls.py
@@ -5,18 +5,18 @@ from recpaper_app.api.views import porject_log, Project_comments
 
 urlpatterns = [
    
-    path('login/', user_login, name='user_login'),
-    path('user/', user_view, name='user_list'),
-    path('ragister/', user_ragister, name='user_ragister'),
+    path('login/', user_login.as_view(), name='user_login'),
+    path('user/', user_view.as_view(), name='user_list'),
+    path('ragister/', user_ragister.as_view(), name='user_ragister'),
     
     
-    path('project/', project_view, name='paper_list'),
-    path('project/<str:pk>/', project_detail, name='project_detail'),
-    path('key/', keyword_view, name='paper_list'),
+    path('project/', project_view.as_view(), name='paper_list'),
+    path('project/<str:pk>/', project_detail.as_view(), name='project_detail'),
+    path('key/', keyword_view.as_view(), name='paper_list'),
     
     
-    path('log/<str:pk>/', porject_log, name='Project_Log'),
-    path('commnets/', Project_comments, name='Project_comments'),
+    path('log/<str:pk>/', porject_log.as_view(), name='Project_Log'),
+    path('commnets/', Project_comments.as_view(), name='Project_comments'),
     
     
     
