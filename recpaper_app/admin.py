b2f87411046d3a2647136f7a46e0d0b164e7910e
Gautam Maurya <gautammaurya1299@gmail.com>
2025-05-20 10:49:45 +0530

    Initialize chat_app with core models, admin registration, and basic structure: Add Messages model for handling chat messages, set up admin interface, and create initial migrations. Include app configuration and placeholder for API views and serializers.

--- recpaper_app/admin.py
+++ recpaper_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from recpaper_app.models import User,Mentor, Project, Project_log, Comment, Files
+from recpaper_app.models import User,Mentor, Project, Project_log, Comment, Files,Faculty
 
 # Register your models here.
 
@@ -9,3 +9,4 @@ admin.site.register(Mentor)
 admin.site.register(Project_log)
 admin.site.register(Comment)
 admin.site.register(Files)
+admin.site.register(Faculty)
